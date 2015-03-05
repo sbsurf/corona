0ce82dbc078f1fa42eae25ee9aea05a0a15dddca
yuri <yuri@nanakdev.com>
2015-03-05 08:47:25 -0800

    Adding boostrap add-on, navbar, styling.

--- Brocfile.js
+++ Brocfile.js
@@ -2,8 +2,11 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
-
+var app = new EmberApp({
+  'ember-cli-bootstrap-sass': {
+    'components': ['bs-nav']
+  }
+});
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
